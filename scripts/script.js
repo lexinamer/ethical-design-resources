e6c6434fa1ae648d2d9bebfbfb44554d30c460b7
Lexi Namer <lexinamer@lexis-mbp.attlocal.net>
2022-06-01 08:57:56 -0400

    Adding search bar for cards

--- scripts/script.js
+++ scripts/script.js
@@ -118,3 +118,13 @@ function matchColor() {
       });
   });
 }
+
+// Search Bar
+$(document).ready(function(){
+  $('.search').on('keyup', function() {
+    var value = $(this).val().toLowerCase();
+    $(".resources .card").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
